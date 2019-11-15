a223891743dd49e2939ef378158bc2cd81b9a1ef
Cesur Kavaslar <cesur.kavaslar@gmail.com>
2019-11-15 13:28:00 -0500

    added getWeather, main, and applyFuzzyRule template functions

--- FuzzyControlSystem.py
+++ FuzzyControlSystem.py
@@ -1,3 +1,6 @@
+# Import statements
+import requests
+
 # Read in API Keys from txt file into dictionary
 def getCredentials():
 	# Get credentials from txt file
@@ -10,4 +13,30 @@ def getCredentials():
 	# Return credentials
 	return credentials
 
+# Get weather information from OpenWeatherMap API
+def getWeather(credentials, location):
+	# Build api string
+	api_call = (
+		"https://api.openweathermap.org/" 
+		+ "data/2.5/weather?q=" + location.replace(" ", "+")
+		+ "&appid=" + credentials["OpenWeatherAPI"] )
+	# Make api call
+	response = requests.get(api_call)
+	print(response)
+	return response
 
+# Apply Fuzzy Rule Set
+def applyFuzzyRules():
+	return
+
+# Main function
+def main():
+	# Get credentials and user's location from input
+	credentials = getCredentials()
+	location = str(input("Enter your location: "))
+	# Get weather for current location
+	weather = getWeather(credentials, location)
+	# Call Fuzzy Rules to decide what to wear!
+	applyFuzzyRules()
+
+main()
