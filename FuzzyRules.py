ab0ed396a89de14cae5f13a752a98ac12b01da87
Cesur Kavaslar <cesur.kavaslar@gmail.com>
2019-12-11 16:22:33 -0500

    push code for Matt;

--- FuzzyRules.py
+++ FuzzyRules.py
@@ -4,47 +4,77 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import time
 
-def ControlSystem(data):
-    return
-    # wind = weather["wind"]["speed"]
-    # temperature =  weather["main"]["temp"]
-    # humidity = weather["main"]["humidity"]
-    # description =  weather[0]["main"]
+# def ControlSystem(data):
+def ControlSystem():
+    # New Antecedent/Consequent objects hold universe variables
+    # and membership functions
+    wind = ctrl.Antecedent(np.arange(0, 25, 1), 'wind')
+    temperature = ctrl.Antecedent(np.arange(-40, 40, 1), 'temperature')
+    humidity = ctrl.Consequent(np.arange(0, 100, 1), 'humidity')
 
-# New Antecedent/Consequent objects hold universe variables
-# and membership functions
-wind = ctrl.Antecedent(np.arange(0, 25, 1), 'wind')
-temperature = ctrl.Antecedent(np.arange(-40, 40, 1), 'temperature')
-humidity = ctrl.Consequent(np.arange(0, 100, 1), 'humidity')
+    # Wind membership functions, Units: Meters per second
+    wind['low'] = fuzz.trimf(wind.universe, [0, 0, 7])
+    wind['medium'] = fuzz.trimf(wind.universe, [0, 5, 10])
+    wind['high'] = fuzz.trimf(wind.universe, [7, 15, 10000])
+    # View wind membership functions
+    # wind['medium'].view()
 
-# Wind membership functions, Units: Meters per second
-wind['low'] = fuzz.trimf(wind.universe, [0, 0, 7])
-wind['medium'] = fuzz.trimf(wind.universe, [0, 5, 10])
-wind['high'] = fuzz.trimf(wind.universe, [7, 15, 10000])
-# View wind membership functions
-wind['medium'].view()
+    # Temperature membership functions
+    temperature['very_cold'] = fuzz.trimf(temperature.universe, [-10000, -25, -20])
+    temperature['cold'] = fuzz.trimf(temperature.universe, [-25, -17.5, -10])
+    temperature['little_cold']  = fuzz.trimf(temperature.universe, [-10, -5, 5])
+    temperature['moderate']  = fuzz.trimf(temperature.universe, [-5, 5, 15])
+    temperature['warm']  = fuzz.trimf(temperature.universe, [10, 15, 20])
+    temperature['hot']  = fuzz.trimf(temperature.universe, [17, 23, 30])
+    temperature['very_hot'] = fuzz.trimf(temperature.universe, [27, 35, 10000])
+    # View temperature membership functions
+    # temperature['moderate'].view()
 
-# Temperature membership functions
-temperature['very_cold'] = fuzz.trimf(temperature.universe, [-10000, -25, -20])
-temperature['cold'] = fuzz.trimf(temperature.universe, [-25, -17.5, -10])
-temperature['litle_cold']  = fuzz.trimf(temperature.universe, [-10, -5, 5])
-temperature['moderate']  = fuzz.trimf(temperature.universe, [-5, 5, 15])
-temperature['warm']  = fuzz.trimf(temperature.universe, [10, 15, 20])
-temperature['hot']  = fuzz.trimf(temperature.universe, [17, 23, 30])
-temperature['very_hot'] = fuzz.trimf(temperature.universe, [27, 35, 10000])
-# View temperature membership functions
-temperature['moderate'].view()
+    # Humidity membership functions
+    humidity['dry'] = fuzz.trimf(humidity.universe, [0, 0, 30])
+    humidity['comfortable'] = fuzz.trimf(humidity.universe, [20, 50, 80])
+    humidity['wet'] = fuzz.trimf(humidity.universe, [50, 80, 10000])
+    # View temperature membership functions
+    # humidity['comfortable'].view()
 
-# Humidity membership functions
-humidity['dry'] = fuzz.trimf(humidity.universe, [0, 0, 30])
-humidity['comfortable'] = fuzz.trimf(humidity.universe, [20, 50, 80])
-humidity['wet'] = fuzz.trimf(humidity.universe, [50, 80, 10000])
-# View temperature membership functions
-humidity['comfortable'].view()
+    bottom(wind, temperature, humidity, 3, 23, 50)
+
+def bottom(wind, temperature, humidity, w, t, h):
+    # Build rules for types of bottoms
+    bottom = ctrl.Consequent(np.arange(0, 26, 1), 'bottom')
+    bottom['cold'] = fuzz.trimf(bottom.universe, [0, 30, 60])
+    bottom['hot'] = fuzz.trimf(bottom.universe, [40, 70, 100])
+
+    # Rules dictating shorts
+    rule1 = ctrl.Rule(
+        (temperature['warm'] | temperature['hot'] | temperature['very_hot'])
+    , bottom['hot'])
+
+    rule2 = ctrl.Rule(
+        humidity['comfortable'] | humidity['wet'], bottom['hot']
+    )
+
+    # Rules dictating pants
+    rule3 = ctrl.Rule(
+        (wind['medium'] | wind['high']), bottom['cold']
+    )
+
+    # Pass rules to controller
+    bottom_ctrl = ctrl.ControlSystem([rule1, rule2])
 
 
+    # bottom_system = ctrl.ControlSystemSimulation(bottom_ctrl)
+    # # Pass in input
+    # bottom_system.input['wind'] = w
+    # bottom_system.input['temperature'] = t
+    # bottom_system.input['humidity'] = h
+    # # Crunch the numbers
+    # bottom_system.compute()
+    # print(bottom_system.output['bottom'])
+    # bottom.view(sim=bottom_system)
 
 
+ControlSystem()
 
 # ----- SAMPLE API RESULT AS REFERENCE -----
 # {
